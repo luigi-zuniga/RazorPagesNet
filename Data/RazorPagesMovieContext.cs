a2c3e3bbcd79ca66dd7f55d8804cca44c10b0830
luigi.zuniga-innova-ti.cl <luis.zuniga@innova-ti.cl>
2019-10-02 12:25:06 -0300

    Initial commit

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace RazorPagesMovies.Data
+{
+    /// DOC-COMMENTS-TAGS
+    /// <summary>
+    /// DbSetpropiedad para el conjunto de entidades
+    /// </summary>
+    /// <remarks>
+    /// Una Instancia DbContext representa una sesion con la base de datos y que puede
+    /// ser usada por QUERY y GUARDAS INSTANCIAS de una Entidad.
+    /// </remarks>
+    /// <example>
+    /// En la terminología de Entity Framework, un conjunto de entidades generalmente corresponde a una tabla de base de datos
+    /// </example>
+
+    public class RazorPagesMovieContext : DbContext
+    {
+
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+        : base(options)
+        {
+        }
+        
+        public DbSet<RazorPagesMovies.Models.Movie> Movie { get; set; }
+    }
+}
+
---
+++ Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovies.Models
+{
+    /// <summary>
+    /// Movie
+    /// </summary>
+    /// <param>
+    /// DataType : se utiliza para asignar DataTypeAttribute,
+    /// es a una sola regla de validación del lado del cliente.
+    /// </param>
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime RealeaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
